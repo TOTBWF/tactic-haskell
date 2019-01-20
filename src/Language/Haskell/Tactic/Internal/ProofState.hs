e6d73473cb2e04a59910a93c1f23fb48d01bc9ec
Reed Mullanix <reedmullanix@gmail.com>
2019-01-19 21:09:58 -0800

    Update to the new, improved ProofStateT.

--- src/Language/Haskell/Tactic/Internal/ProofState.hs
+++ src/Language/Haskell/Tactic/Internal/ProofState.hs
@@ -11,42 +11,54 @@
 -- This module provides the standard LCF definition of a proof state.
 -- However, there are a couple of interesting points. Namely, @'ProofState' jdg@
 -- is parameterized, which means that @'ProofState'@ becomes a @'Monad'@!
-{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Language.Haskell.Tactic.Internal.ProofState
-  ( ProofState(..)
+  ( ProofStateT(..)
   , axiom
+  , subgoal
   ) where
 
+-- import Language.Haskell.TH.Ppr
+-- import Language.Haskell.TH.PprLib hiding (empty, (<>))
+-- import qualified Language.Haskell.TH.PprLib as P
+
+import Data.Functor.Alt
+import Control.Monad
+import Control.Monad.Except
+import Control.Monad.Trans
+import Control.Monad.IO.Class
+
+import Pipes.Core
+
 import Language.Haskell.TH
-import Language.Haskell.TH.Ppr
-import Language.Haskell.TH.PprLib hiding (empty, (<>))
-import qualified Language.Haskell.TH.PprLib as P
 
-import Control.Monad(ap)
+newtype ProofStateT m jdg = ProofStateT { unProofStateT :: Client jdg Exp m Exp }
 
--- | A @'ProofState'@ is a list of subgoals of type @jdg@, along
--- with a function that takes the extract of each of the subgoals,
--- and creates a new extract.
-data ProofState jdg = ProofState [jdg] ([Exp] -> Exp)
-  deriving (Functor, Foldable, Traversable)
+instance (Monad m) => Functor (ProofStateT m) where
+  fmap f (ProofStateT p) = ProofStateT $ (request . f) >\\ p
 
-instance (Ppr jdg) => Ppr (ProofState jdg) where
-  ppr (ProofState subgoals _) =
-    vcat $ fmap (\(i,sg) -> int i P.<> text ")" <+> ppr sg) $ zip [1 ..] subgoals
+instance (MonadError e m) => Alt (ProofStateT m) where
+  (ProofStateT p1) <!> (ProofStateT p2) = ProofStateT $ p1 `catchError` (const p2)
 
--- | Creates a proof state with no subgoals.
-axiom :: Exp -> ProofState jdg
-axiom e = ProofState [] (const e)
+instance (Monad m) => Applicative (ProofStateT m) where
+  pure a = ProofStateT $ request a
+  (ProofStateT pf) <*> (ProofStateT pa) = ProofStateT $ (\f -> (request . f) >\\ pa) >\\ pf
 
-instance Applicative ProofState where
-  pure j = ProofState [j] head
-  (<*>) = ap
-
-instance Monad ProofState where
+instance (Monad m) => Monad (ProofStateT m) where
   return = pure
-  (ProofState subgoals ext) >>= k =
-    let build (ProofState s e) (sg, ext) =
-          let n = length s
-          in (s <> sg, \es -> (e $ take n es):(ext $ drop n es))
-        (subgoals', ext') = foldr build ([], id) $ k <$> subgoals
-    in ProofState subgoals' (ext . ext')
+  (ProofStateT p) >>= k = ProofStateT $ (unProofStateT . k) >\\ p
+
+instance MonadTrans (ProofStateT ) where
+  lift m = ProofStateT $ request =<< (lift m)
+
+instance (MonadIO m) => MonadIO (ProofStateT m) where
+  liftIO m = ProofStateT $ request =<< (liftIO m)
+
+-- | Create a @'ProofState'@ with no subgoals.
+axiom :: (Monad m) => Exp -> ProofStateT m jdg
+axiom e = ProofStateT $ return e
+
+-- | Creates a @'ProofState'@ with a subgoal.
+subgoal :: (Monad m) => jdg -> ProofStateT m jdg
+subgoal j = ProofStateT $ request j
