26362996b574e0f18e17b7e19e9e0cc135039179
Reed Mullanix <reed.mullanix@mybazinga.com>
2019-07-06 15:34:03 -0700

    Migrate to Refinery + GHC API

--- src/Language/Haskell/Tactic/Patterns.hs
+++ src/Language/Haskell/Tactic/Patterns.hs
@@ -8,30 +8,43 @@
 --
 -- =TH
 -- This module exports some handy TH AST pattern synonyms
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE TemplateHaskell #-}
-module Language.Haskell.Tactic.Internal.TH
-  ( pattern Arrow
-  , pattern Function
+module Language.Haskell.Tactic.Patterns
+  ( pattern Function
   , pattern Tuple
   , pattern Constructor
+  , Expr
   , DCon(..)
+  , var
+  , lam
+  , tuple
+  , app
   ) where
 
-import Language.Haskell.TH
+import BasicTypes
+import Name
+import HsBinds
+import HsExpr
+import HsExtension
+import HsPat
+import SrcLoc
+import TyCon
+import TyCoRep
+import Var
 
--- | Pattern for a single function arrow
-pattern Arrow :: Type -> Type -> Type
-pattern Arrow t1 t2 = AppT (AppT ArrowT t1) t2
+type Expr = HsExpr (GhcPass 'Typechecked)
 
 function :: Type -> Maybe ([Type], Type)
-function (Arrow t1 t2) =
+function (ForAllTy _ t) = function t
+function (FunTy t1 t2) =
   let ts = go t2
   in Just $ (t1:init ts, last ts)
   where
     go :: Type -> [Type]
-    go (Arrow t1 t2) = t1:go t2
+    go (FunTy t1 t2) = t1:go t2
     go t = [t]
 function _ = Nothing
 
@@ -39,29 +52,17 @@ function _ = Nothing
 pattern Function :: [Type] -> Type -> Type
 pattern Function args ret <- (function -> Just (args, ret))
 
-tuple :: Type -> Maybe [Type]
-tuple = go []
-  where
-    go :: [Type] -> Type -> Maybe [Type]
-    go ts (AppT (TupleT i) t) =
-      let ts' = t:ts
-      in (if length ts' == i then Just ts' else Nothing)
-    go ts (AppT t1 t2) = go (t2:ts) t1
-    go _ _ = Nothing
+tupleTy :: Type -> Maybe [Type]
+tupleTy (TyConApp tycon ts) | isTupleTyCon tycon = Just ts
+tupleTy _ = Nothing
 
 -- | Pattern for a tuple of any given arity
 pattern Tuple :: [Type] -> Type
-pattern Tuple ts <- (tuple -> Just ts)
+pattern Tuple ts <- (tupleTy -> Just ts)
 
 constructor :: Type -> Maybe (Name, [Type])
-constructor (ConT n) = Just (n, [])
-constructor (AppT ListT t) = Just (''[], [t])
-constructor ty = go [] ty
-  where
-    go :: [Type] -> Type -> Maybe (Name, [Type])
-    go ts (AppT (ConT n) t) = Just (n, t:ts)
-    go ts (AppT t1 t2) = go (t2:ts) t1
-    go _ _ = Nothing
+constructor (TyConApp tycon tvs) = Just (tyConName tycon, tvs)
+constructor _ = Nothing
 
 -- | Pattern for a constructor application
 pattern Constructor :: Name -> [Type] -> Type
@@ -69,3 +70,17 @@ pattern Constructor n ts  <- (constructor -> Just (n, ts))
 
 -- | Simple Data Constructor Type
 data DCon = DCon Name [Type]
+
+var :: Var -> Expr
+var v = HsVar (noLoc v)
+
+lam :: Var -> Type -> Type -> Expr -> Expr
+lam v a b body =
+  let rhs = GRHSs [noLoc $ GRHS [] (noLoc body)] (noLoc EmptyLocalBinds)
+  in HsLam $ MG (noLoc [noLoc $ Match LambdaExpr [noLoc (VarPat (noLoc v))] rhs]) [a] b Generated
+
+tuple :: [Expr] -> Expr
+tuple exprs = ExplicitTuple (fmap (noLoc . Present . noLoc) exprs) Boxed
+
+app :: Expr -> Expr -> Expr
+app e1 e2 = HsApp (noLoc e1) (noLoc e2)
